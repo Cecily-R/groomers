4174d009776350b66c39e57d2b0091a010a1d05d
Cecily Rowland <cec1rowland@gmail.com>
2023-06-14 09:22:27 +0100

    Added Bootstrap to homepage

--- frontend/src/components/login/LogInForm.js
+++ frontend/src/components/login/LogInForm.js
@@ -1,10 +1,31 @@
+import "./LogInForm.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Form, Button, Container } from 'react-bootstrap'
+
 const LogInForm = () => {
 
-    return (
-      <div>
-        <h2>Log In Here</h2>
+  return (
+    <div className="form-container" style={{ display: 'block', 
+                  width: 700, 
+                  padding: 50, 
+                  }}>
+        <Form>
+          <Form.Group className="mb-3" controlId="formGroupEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>    
+
+          <Form.Group className="mb-3" controlId="formGroupPassword">
+            <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+
+          <Button variant="outline-success" type="submit">
+            Submit
+          </Button>   
+      </Form>
       </div>
-    )
-  }
+  )
+}
 
 export default LogInForm;
